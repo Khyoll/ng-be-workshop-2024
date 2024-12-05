a4e2394360e5de3428d19e4282a91ba997f56c04
= <remyvancaekenberghe@gmail.com>
2024-12-05 15:16:16 +0100

    add executors for apps

---
+++ apps/movies-app/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://nx-workshop-2024-revc-fragrant-star-8870.fly.dev/',
+  };
---
+++ libs/internal-plugin/src/executors/fly-deploy/executor.ts
@@ -0,0 +1,28 @@
+import { PromiseExecutor } from '@nx/devkit';
+import { FlyDeployExecutorSchema } from './schema';
+import { execSync } from 'child_process';
+
+const runExecutor: PromiseExecutor<FlyDeployExecutorSchema> = async (
+  options
+) => {
+  const cwd = options.distLocation;
+  const results = execSync(`fly apps list`, { encoding: 'utf-8' });
+  console.log(results);
+  try {
+    if (results.toString().includes(options.flyAppName)) {
+      execSync(`flyctl deploy`, { cwd, stdio: 'inherit' });
+    } else {
+      // consult https://fly.io/docs/reference/regions/ to get best region for you
+      execSync(`flyctl launch --now --name=${options.flyAppName} --yes --copy-config --region=lax`, {
+        cwd,
+        stdio: 'inherit',
+      });
+    }
+    return { success: true };
+  } catch (error) {
+    console.error('Deployment failed:', error);
+    return { success: false };
+  }
+};
+
+export default runExecutor;
