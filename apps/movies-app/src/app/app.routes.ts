80bb878484f340c26a40dc799685fbb198251b19
= <remyvancaekenberghe@gmail.com>
2024-12-05 16:40:45 +0100

    update movies title

--- apps/movies-app/src/app/app.routes.ts
+++ apps/movies-app/src/app/app.routes.ts
@@ -7,7 +7,7 @@ export const routes: Routes = [
       import('@nx-workshop/movies/feature-movie-list').then(
         m => m.MovieListPageComponent
       ),
-    title: 'Movies List'
+    title: 'Movies List updated'
   },
   {
     path: 'list/genre/:id',
